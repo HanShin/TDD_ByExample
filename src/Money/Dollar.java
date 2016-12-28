8ea781be13ed394ef4241353cc0bbb0245632725
한신 <shin86@wemakeprice.com>
2016-12-28 18:41:48 +0900

    일단 1차 테스트 코드 작성

---
+++ src/Money/Dollar.java
@@ -0,0 +1,15 @@
+package Money;
+
+/**
+ * Created by shin on 2016. 12. 28..
+ */
+public class Dollar {
+    int amount;
+    Dollar(int ammount){
+
+    }
+
+    void times(int multiplier){
+
+    }
+}
--- src/Money/MoneyTest.java
+++ src/Money/MoneyTest.java
@@ -1,7 +1,19 @@
 package Money;
 
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
 /**
  * Created by shin on 2016. 12. 28..
  */
+@Test
 public class MoneyTest {
+
+    public void testMultiplication(){
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
+
 }
