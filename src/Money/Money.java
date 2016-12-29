f0feea4f15f5c98562cfee7f21d6f4bfdf9dc4c3
한신 <shin86@wemakeprice.com>
2016-12-29 17:53:19 +0900

    Franc 클래스와 Dollar클래스 삭제하고
    Money 클래스 하나로 통일

--- src/Money/Money.java
+++ src/Money/Money.java
@@ -5,9 +5,36 @@ package Money;
  */
 public class Money {
     protected int amount;
+    protected String currency;
+
+    Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
+
+    Money(int amount, String currency){
+        this.amount = amount;
+        this.currency = currency;
+    }
 
     public boolean equals(Object object){
         Money money = (Money) object;
-        return amount == money.amount;
+        return amount == money.amount && currency().equals(money.currency());
     }
+
+    public static Money dollar(int amount){
+        return new Money(amount, "USD");
+    }
+
+    public static Money franc(int amount){
+        return new Money(amount, "CHF");
+    }
+
+    public String currency(){
+        return currency;
+    }
+
+    public String toString(){
+        return amount + " " + currency;
+    }
+
 }
