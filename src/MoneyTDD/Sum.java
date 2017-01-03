14a8c8d26b87774a8209d206b7301cd9d20656d4
한신 <shin86@wemakeprice.com>
2017-01-03 19:07:25 +0900

    추상화직전단계

--- src/MoneyTDD/Sum.java
+++ src/MoneyTDD/Sum.java
@@ -3,22 +3,23 @@ package MoneyTDD;
 /**
  * Created by shin on 2017. 1. 2..
  */
-public class Sum implements Expression{
+public class Sum implements Expression {
     Expression augend;
     Expression addend;
 
-    Sum (Expression augend, Expression addend){
+    Sum(Expression augend, Expression addend) {
         this.augend = augend;
         this.addend = addend;
     }
 
     @Override
     public Money reduce(Bank bank, String to) {
-        int amount = augend.reduce(bank, to).amount + addend.reduce(bank,to).amount;
-        return new Money (amount, to);
+        int amount = augend.reduce(bank, to).amount + addend.reduce(bank, to).amount;
+        return new Money(amount, to);
     }
 
     @Override
     public Expression plus(Expression addend) {
-        return new Sum(this,addend);
-    }}
+        return new Sum(this, addend);
+    }
+}
